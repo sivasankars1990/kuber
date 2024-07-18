91e8687321ca71653cf4e28b53b0dc5fd51ab679
SIVA <Sivasankars1990@gmail.com>
2024-07-18 19:14:29 +0530

    first commit

---
+++ src/main/java/dev/siva_demo/SivaDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.siva_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/siva_demo/SivaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.siva_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SivaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
